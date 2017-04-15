60a361178039e06238bbe05c887a9a6a5f7e53c5
kota718 <kota.yt@gmail.com>
2017-04-15 23:52:31 +0900

    I18n対応した

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 gem 'devise', '4.2.1'
+gem 'http_accept_language', '~> 2.1'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  private
+
+    def set_locale
+      I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+      Rails.logger.debug "I18n.locale: #{I18n.locale}"
+    end
 end
